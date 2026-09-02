c6ff74dd36c77d3130fc0221afc156767d5ebed8
maeda0404 <bpo-test@ml.obayashi.co.jp>
2026-09-02 16:16:27 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,2 @@
+'use strict';
+window.SIGNAGE_CONFIG=Object.freeze({dataUrl:'./data/current.json',refreshMs:300000,staleMs:3600000,rotationMs:20000,scheduledStartMinute:0,scheduledEndMinute:10,thresholds:Object.freeze({rainProbability:60,lowTemperature:5,strongWind:10,heavyRainPerHour:10})});
